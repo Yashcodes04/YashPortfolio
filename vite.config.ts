e0e6bce53b4852bbe8aad81a3975cba461959fbb
Yashcodes04 <yashgawhale004@gmail.com>
2025-08-16 09:59:55 +0530

    prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Yash/', // ⚡ add this if hosting on GitHub Pages
+  base: '/YashPortfolio/', // ⚡ add this if hosting on GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     include: ['lucide-react'],
